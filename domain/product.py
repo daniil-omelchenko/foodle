6d29126fff29193ee9df19675e5b4254cd0e0559
Daniel <dpixelstudio@gmail.com>
2018-10-20 15:04:00 +0300

    product domain object changes

--- domain/product.py
+++ domain/product.py
@@ -3,15 +3,13 @@ from domain.serializable import Serializable
 
 class Product(Serializable):
 
-    def __init__(self, account, product_name, product_id, photo_origin):
-        self.account_id = account
+    def __init__(self, product_name, product_id, photo_origin):
         self.product_name = product_name
         self.product_id = product_id
         self.photo_origin = photo_origin
 
     def serialize(self):
         return {
-            'account': self.account_id,
             'product_name': self.product_name,
             'product_id': self.product_id,
             'photo_origin': self.photo_origin
@@ -19,11 +17,7 @@ class Product(Serializable):
 
     def deserialize(self, data):
         return Product(
-            account=None,
             product_name=data['product_name'],
             product_id=data['product_id'],
             photo_origin=data['photo_origin']
         )
-
-    def set_account(self, account):
-        self.account_id = account
