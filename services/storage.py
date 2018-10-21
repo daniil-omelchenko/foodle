ac8a6e26a342dd49b5d984e944a9b34bdf4ca6b6
Stepan Mikhaylov <stepanmikhaylov.sprinklebit@gmail.com>
2018-10-21 13:43:34 +0300

    added bot handlers and search methods

---
+++ services/storage.py
@@ -0,0 +1,8 @@
+def get_products(search_request):
+    #ToDo: return list of product model
+    return None
+
+
+def find_related_modles(product):
+    #ToDo: return product, spot, account, spotproduct models
+    return None
